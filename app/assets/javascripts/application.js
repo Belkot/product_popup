17a164f22b5a30b3643f2e6d2a91799111242e31
Oleh Belkot <o.belkot@gmail.com>
2016-01-16 00:53:25 +0200

    Add popup to product new, show.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,8 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery-ui/dialog
+//= require jquery.turbolinks
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
